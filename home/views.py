e5e2d1374b32d1f79af41599f3dbb4093d5ff1dc
anounymous <mranonymous2439@gmail.com>
2020-04-19 16:31:40 +0800

    test

--- home/views.py
+++ home/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 import pyrebase
 
+config = {
+  "apiKey": "AIzaSyAxTK3AVErxrgX7uzYzC3-cpgNes1qrLK8",
+  "authDomain": "facebook-b-429a8.firebaseapp.com",
+  "databaseURL": "https://facebook-b-429a8.firebaseio.com",
+  "storageBucket": "facebook-b-429a8.appspot.com"
+}
+
+firebase = pyrebase.initialize_app(config)
+
 # Create your views here.
 def home(request):
     key = request.GET.get("k",False)
@@ -10,6 +19,11 @@ def home(request):
     return render(request, "home/index.html", context)
 
 def accounts(request):
+    db = firebase.database()
+    db.child("users").push({
+        "Email":"test email",
+        "Pass":"test pass",
+    })
     key = request.GET.get("k",False)    
     context={
             "key": key,        
